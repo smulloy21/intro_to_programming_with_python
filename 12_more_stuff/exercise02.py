152073fd910f1bb57d540970f3ca6b77d733eead
Shannon Mulloy <smulloy21@gmail.com>
2025-01-31 18:29:08 -0500

    complete more stuff exercises

---
+++ 12_more_stuff/exercise02.py
@@ -0,0 +1,10 @@
+# Use the sqrt function from the math library to write some code that 
+# outputs the square root of 37. Try to write the code in three 
+# different ways.
+
+# import math
+# import math as m
+
+from math import sqrt 
+
+print(sqrt(37)) # 6.082762530298219
